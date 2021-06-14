f861a3d71a16f7c6e5995d04d1358e5d0375c74c
BASTIAN NICOLAS NUNEZ LILLO <bastian.nunez16@inacapmail.cl>
2021-06-14 12:26:49 -0400

    Category Model

---
+++ CookieFactory/Models/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieFactory.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Category Name Required")]
+        [StringLength(40, ErrorMessage = "{0} must be between {2} and {1} characters", MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Category Description Required")]
+        public string Description { get; set; }
+
+
+        public Category()
+        {
+
+        }
+    }
+}
