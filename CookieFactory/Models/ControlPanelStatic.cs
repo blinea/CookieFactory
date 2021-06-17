7d367ae39d2dc769bd8bbfeb8ca90811e9a86a2f
BASTIAN NICOLAS NUNEZ LILLO <bastian.nunez16@inacapmail.cl>
2021-06-16 20:01:28 -0400

    Static Boys

---
+++ CookieFactory/Models/ControlPanelStatic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieFactory.Models
+{
+    public static class ControlPanelStatic
+    {
+        public static string Cookies()
+        {
+            string name = "Cookies";
+            return name;
+        }
+
+        public static string Categories()
+        {
+            string name = "Categories";
+            return name;
+        }
+
+        public static string Products()
+        {
+            string name = "Products";
+            return name;
+        }
+
+        public static DateTime Now()
+        {
+            DateTime dateTime = DateTime.Now;
+            return dateTime;
+        }
+    }
+}
