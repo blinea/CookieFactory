129e42ba6c8ddf92b582bc590fe63207bf54dd67
BASTIAN NICOLAS NUNEZ LILLO <bastian.nunez16@inacapmail.cl>
2021-07-06 19:20:57 -0400

    Last Commit?

--- CookieFactory/Controllers/EmailsController.cs
+++ CookieFactory/Controllers/EmailsController.cs
@@ -73,6 +73,7 @@ namespace CookieFactory.Controllers
             {
 
                 string wwwRootPath = _hostEnviroment.WebRootPath;
+            
                 string filename = Path.GetFileNameWithoutExtension(email.AttatchmentFile.FileName);
                 string extension = Path.GetExtension(email.AttatchmentFile.FileName);
                 email.Attatchment = filename = filename + DateTime.Now.ToString("yymmssfff") + extension;
@@ -81,13 +82,15 @@ namespace CookieFactory.Controllers
                 {
                     await email.AttatchmentFile.CopyToAsync(fileStream);
                 }
-
+                
+                
                 MailMessage mail = new MailMessage();
 
                 mail.From = new MailAddress("cookiefactoryenterprise@gmail.com");
                 mail.To.Add("cookiefactoryenterprise@gmail.com");
                 mail.Subject = email.UserName + " - " + email.UserEmail;
                 mail.Body = email.Message;
+
                 mail.Attachments.Add(new Attachment(path));
 
                 SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
@@ -98,6 +101,7 @@ namespace CookieFactory.Controllers
                     delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
                     { return true; };
                 smtpServer.Send(mail);
+                smtpServer.Dispose();
 
                 _context.Add(email);
                 await _context.SaveChangesAsync();
@@ -105,6 +109,61 @@ namespace CookieFactory.Controllers
             }
             return View(email);
         }
+
+        // POST: Emails/Reply
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Reply([Bind("EmailId,UserName,UserEmail,Message,AttatchmentFile")] Email email)
+        {
+            if (ModelState.IsValid)
+            {
+
+                string wwwRootPath = _hostEnviroment.WebRootPath;
+
+                string filename = Path.GetFileNameWithoutExtension(email.AttatchmentFile.FileName);
+                string extension = Path.GetExtension(email.AttatchmentFile.FileName);
+                email.Attatchment = filename = filename + DateTime.Now.ToString("yymmssfff") + extension;
+                string path = Path.Combine(wwwRootPath + "/Attatchments/", filename);
+                using (var fileStream = new FileStream(path, FileMode.Create))
+                {
+                    await email.AttatchmentFile.CopyToAsync(fileStream);
+                }
+
+                
+
+
+                MailMessage mail = new MailMessage();
+
+                mail.From = new MailAddress("cookiefactoryenterprise@gmail.com");
+                mail.To.Add(email.UserEmail);
+                mail.Subject = "Reply to your Request";
+                mail.Body = email.Message;
+
+                mail.Attachments.Add(new Attachment(path));
+
+                SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+                smtpServer.Port = 587;
+                smtpServer.Credentials = new System.Net.NetworkCredential("cookiefactoryenterprise@gmail.com", "cookiefactory666") as ICredentialsByHost;
+                smtpServer.EnableSsl = true;
+                ServicePointManager.ServerCertificateValidationCallback =
+                    delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+                    { return true; };
+                smtpServer.Send(mail);
+                smtpServer.Dispose();
+
+                email.UserName = "Admin";
+                email.UserEmail = "cookiefactoryenterprise@gmail.com";
+
+                _context.Add(email);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("ControlPanel", "Home");
+            }
+            return View(email);
+        }
+
 
         // GET: Emails/Edit/5
         [Authorize]
